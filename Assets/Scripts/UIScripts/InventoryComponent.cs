013e4779628890a5278f1091c0de01b0b9084ba3
Robert Palermo <robert.palermo.official@gmail.com>
2022-04-17 17:59:53 -0400

    Added Gun Models and Added Pickups to Map

--- Assets/Scripts/UIScripts/InventoryComponent.cs
+++ Assets/Scripts/UIScripts/InventoryComponent.cs
@@ -20,12 +20,12 @@ public class InventoryComponent : MonoBehaviour
 
     public ItemScript FindItem(string itemName)
     {
-        return Items.Find((invItem) => invItem.name == itemName);
+        return Items.Find((invItem) => invItem.itemName == itemName);
     }
 
     public void AddItem(ItemScript item, int amount = 0)
     {
-        int itemIndex = Items.FindIndex(listItem => listItem.name == item.name);
+        int itemIndex = Items.FindIndex(listItem => listItem.itemName == item.itemName);
         if (itemIndex != -1)
         {
             ItemScript listItem = Items[itemIndex];
@@ -47,8 +47,8 @@ public class InventoryComponent : MonoBehaviour
 
     public void DeleteItem(ItemScript item)
     {
-        Debug.Log($"{item.name} deleted!");
-        int itemIndex = Items.FindIndex(listItem => listItem.name == item.name);
+        Debug.Log($"{item.itemName} deleted!");
+        int itemIndex = Items.FindIndex(listItem => listItem.itemName == item.itemName);
         if (itemIndex == -1) return;
 
         Items.Remove(item);
